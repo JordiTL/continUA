a5feecdb2af8d4bbc9eb2f48b796adc09236622c
Jorge Torregrosa <jtorregrosalloret@gmail.com>
2016-03-05 15:13:15 +0100

    Initialized project base

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name continuaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the continuaApp
+ */
+angular.module('continuaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name continuaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the continuaApp
+ */
+angular.module('continuaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
