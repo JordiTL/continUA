3bca9770a55acc2664fc1d5f37b174a5b3e9bd03
paco <openbook@first-asd.eu>
2016-03-07 16:57:11 +0100

    Created login view and controller

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name continuaApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the continuaApp
+ */
+angular.module('continuaApp')
+  .controller('LoginCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
